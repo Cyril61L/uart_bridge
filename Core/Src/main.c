6b4a6a0beddd827056ba4f8ef6a7762008c61fca
lemaricy <cyril.lemarie@reseau.eseo.fr>
2022-07-02 00:00:19 +0200

    use FreeRTOS & streamBuffer

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,32 +18,28 @@
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
-
 #include "main.h"
+#include "cmsis_os.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include <stdbool.h>
 #include <string.h>
+#include <task.h>
+#include "stream_buffer.h"
 //#include <stdio.h>
 
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-#define BUFFER_SIZE 256
+#define STORAGE_SIZE_BYTES 1024
 
-typedef struct {
-    uint8_t rxBuffer[BUFFER_SIZE];
-    uint8_t txBuffer[BUFFER_SIZE];
-    uint32_t messageSize;
-    uint32_t count;
-    volatile bool flag;
-}uartBuffer_t;
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -53,22 +49,36 @@ typedef struct {
 
 /* Private variables ---------------------------------------------------------*/
 
+/* Definitions for defaultTask */
+osThreadId_t defaultTaskHandle;
+const osThreadAttr_t defaultTask_attributes = {
+  .name = "defaultTask",
+  .priority = (osPriority_t) osPriorityNormal,
+  .stack_size = 128 * 4
+};
+/* Definitions for uart1Task */
+osThreadId_t uart1TaskHandle;
+const osThreadAttr_t uart1Task_attributes = {
+  .name = "uart1Task",
+  .priority = (osPriority_t) osPriorityLow,
+  .stack_size = 512 * 4
+};
+/* Definitions for uart2Task */
+osThreadId_t uart2TaskHandle;
+const osThreadAttr_t uart2Task_attributes = {
+  .name = "uart2Task",
+  .priority = (osPriority_t) osPriorityLow,
+  .stack_size = 512 * 4
+};
 /* USER CODE BEGIN PV */
-uartBuffer_t uart1 = {
-        .rxBuffer = {0},
-        .txBuffer = {0},
-        .messageSize = 0,
-        .count = 0,
-        .flag = false,
-};
 
-uartBuffer_t uart2 = {
-        .rxBuffer = {0},
-        .txBuffer = {0},
-        .messageSize = 0,
-        .count = 0,
-        .flag = false,
-};
+static uint8_t uart1Buffer[ STORAGE_SIZE_BYTES ];
+static uint8_t uart2Buffer[ STORAGE_SIZE_BYTES ];
+StreamBufferHandle_t uart1StreamBufferHandle;
+StreamBufferHandle_t uart2StreamBufferHandle;
+StaticStreamBuffer_t xStreamBufferStruct1;
+StaticStreamBuffer_t xStreamBufferStruct2;
+
 
 /* USER CODE END PV */
 
@@ -77,30 +87,33 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_USART2_UART_Init(void);
+void StartDefaultTask(void *argument);
+void StartUart1Task(void *argument);
+void StartUart2Task(void *argument);
+
 /* USER CODE BEGIN PFP */
 /**
   * @brief This function handles USART1 global interrupt.
   */
 void USART1_IRQHandler(void)
 {
+    char cC;
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE; /* Initialised to pdFALSE. */
+
     /** Received byte event **/
-   if(LL_USART_IsActiveFlag_RXNE(USART1))
-   {
-       LL_USART_ClearFlag_RXNE(USART1);
-       if(uart1.count < BUFFER_SIZE - 1) {
-           uart1.rxBuffer[uart1.count] = LL_USART_ReceiveData8(USART1);
-           uart1.count++;
-       }
-       LL_USART_EnableIT_IDLE(USART1);
-   }
+    if(LL_USART_IsActiveFlag_RXNE(USART1))
+    {
+        cC = LL_USART_ReceiveData8(USART1);
+        xStreamBufferSendFromISR(uart1StreamBufferHandle,&cC,1,&xHigherPriorityTaskWoken);
+        LL_USART_EnableIT_IDLE(USART1);
+    }
     /** Idle event **/
-   if(LL_USART_IsActiveFlag_IDLE(USART1))
-   {
-       LL_USART_DisableIT_IDLE(USART1);
-       uart1.flag = true;
-       uart1.messageSize = uart1.count;
-       uart1.count = 0;
-   }
+    if(LL_USART_IsActiveFlag_IDLE(USART1))
+    {
+        LL_USART_DisableIT_IDLE(USART1);
+        vTaskNotifyGiveFromISR(uart1TaskHandle,&xHigherPriorityTaskWoken);
+    }
+    portYIELD_FROM_ISR(&xHigherPriorityTaskWoken);
 }
 
 /**
@@ -108,40 +121,44 @@ void USART1_IRQHandler(void)
   */
 void USART2_IRQHandler(void)
 {
+    char cC;
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE; /* Initialised to pdFALSE. */
+
     /** Received byte event **/
     if(LL_USART_IsActiveFlag_RXNE(USART2))
     {
         LL_USART_ClearFlag_RXNE(USART2);
-        if(uart2.count < BUFFER_SIZE - 1)
-        {
-            uart2.rxBuffer[uart2.count] = LL_USART_ReceiveData8(USART2);
-            uart2.count ++;
-        }
+        /** Read the uart rx (DR) register **/
+        cC = LL_USART_ReceiveData8(USART2);
+        /** Push the char to the stream buffer **/
+        xStreamBufferSendFromISR(uart2StreamBufferHandle,&cC,1,&xHigherPriorityTaskWoken);
+        /** Enable IT to detect the of trame **/
         LL_USART_EnableIT_IDLE(USART2);
     }
     /** Idle event **/
     if(LL_USART_IsActiveFlag_IDLE(USART2))
     {
         LL_USART_DisableIT_IDLE(USART2);
-        uart2.flag = true;
-        uart2.messageSize = uart2.count;
-        uart2.count = 0;
+        /** Notify the task that end of trame event **/
+        vTaskNotifyGiveFromISR(uart2TaskHandle,&xHigherPriorityTaskWoken);
     }
+    portYIELD_FROM_ISR(&xHigherPriorityTaskWoken);
 }
 
 /**
- * @brief
+ * @brief Send data to UART without interrupt.
  * @param USARTx
  * @param rxBuffer
- * @param size
+ * @param size Len of rxBuffer
  */
-void UART_Send(USART_TypeDef *USARTx,const uint8_t *rxBuffer,uint8_t size){
-    uint32_t buffer[size];
-    memcpy(buffer,rxBuffer,size);
+void UART_Send(USART_TypeDef *USARTx,const uint8_t *txBuffer,uint8_t size){
+    uint8_t buffer[size];
+    memcpy(buffer,txBuffer,size);
 
     for(uint32_t i=0; i<size;i++)
     {
         LL_USART_TransmitData8(USARTx,buffer[i]);
+        /** Wait the char is transfering to the shift register **/
         while(!LL_USART_IsActiveFlag_TXE(USARTx));
     }
 }
@@ -160,26 +177,16 @@ void UART_Send(USART_TypeDef *USARTx,const uint8_t *rxBuffer,uint8_t size){
 int main(void)
 {
   /* USER CODE BEGIN 1 */
+    const size_t xTriggerLevel = 1;
 
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-
-  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_AFIO);
-  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
-
-  NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
-
-  /* System interrupt init*/
-
-  /** NOJTAG: JTAG-DP Disabled and SW-DP Enabled
-  */
-  LL_GPIO_AF_Remap_SWJ_NOJTAG();
+  HAL_Init();
 
   /* USER CODE BEGIN Init */
-
   /* USER CODE END Init */
 
   /* Configure the system clock */
@@ -198,26 +205,65 @@ int main(void)
     UART_Send(USART1,"Start message USART1\r\n",22);
   /* USER CODE END 2 */
 
+  /* Init scheduler */
+  osKernelInitialize();
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+    uart1StreamBufferHandle = xStreamBufferCreateStatic( sizeof( uart1Buffer ),
+                                               xTriggerLevel,
+                                               uart1Buffer,
+                                               &xStreamBufferStruct1 );
+
+    uart2StreamBufferHandle = xStreamBufferCreateStatic( sizeof( uart2Buffer ),
+                                                         xTriggerLevel,
+                                                         uart2Buffer,
+                                                         &xStreamBufferStruct2 );
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of defaultTask */
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* creation of uart1Task */
+  uart1TaskHandle = osThreadNew(StartUart1Task, NULL, &uart1Task_attributes);
+
+  /* creation of uart2Task */
+  uart2TaskHandle = osThreadNew(StartUart2Task, NULL, &uart2Task_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-      if(uart1.flag)
-      {
-          uart1.flag = false;
-          UART_Send(USART2,uart1.rxBuffer,uart1.messageSize);
-          uart1.messageSize = 0;
-      }
-      if(uart2.flag)
-      {
-          uart2.flag = false;
-          UART_Send(USART1,uart2.rxBuffer,uart2.messageSize);
-          uart2.messageSize = 0;
-      }
+    while (1)
+    {
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-  }
+    }
   /* USER CODE END 3 */
 }
 
@@ -231,15 +277,15 @@ void SystemClock_Config(void)
   while(LL_FLASH_GetLatency()!= LL_FLASH_LATENCY_2)
   {
   }
-  LL_RCC_HSI_SetCalibTrimming(16);
-  LL_RCC_HSI_Enable();
+  LL_RCC_HSE_EnableBypass();
+  LL_RCC_HSE_Enable();
 
-   /* Wait till HSI is ready */
-  while(LL_RCC_HSI_IsReady() != 1)
+   /* Wait till HSE is ready */
+  while(LL_RCC_HSE_IsReady() != 1)
   {
 
   }
-  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI_DIV_2, LL_RCC_PLL_MUL_16);
+  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE_DIV_1, LL_RCC_PLL_MUL_8);
   LL_RCC_PLL_Enable();
 
    /* Wait till PLL is ready */
@@ -257,8 +303,13 @@ void SystemClock_Config(void)
   {
 
   }
-  LL_Init1msTick(64000000);
   LL_SetSystemCoreClock(64000000);
+
+   /* Update the time base */
+  if (HAL_InitTick (TICK_INT_PRIORITY) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /**
@@ -296,7 +347,7 @@ static void MX_USART1_UART_Init(void)
   LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* USART1 interrupt Init */
-  NVIC_SetPriority(USART1_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),0, 0));
+  NVIC_SetPriority(USART1_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),5, 0));
   NVIC_EnableIRQ(USART1_IRQn);
 
   /* USER CODE BEGIN USART1_Init 1 */
@@ -354,7 +405,7 @@ static void MX_USART2_UART_Init(void)
   LL_GPIO_Init(USART_RX_GPIO_Port, &GPIO_InitStruct);
 
   /* USART2 interrupt Init */
-  NVIC_SetPriority(USART2_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),0, 0));
+  NVIC_SetPriority(USART2_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),5, 0));
   NVIC_EnableIRQ(USART2_IRQn);
 
   /* USER CODE BEGIN USART2_Init 1 */
@@ -371,7 +422,6 @@ static void MX_USART2_UART_Init(void)
   LL_USART_ConfigAsyncMode(USART2);
   LL_USART_Enable(USART2);
   /* USER CODE BEGIN USART2_Init 2 */
-    LL_USART_EnableIT_RXNE(USART2);
 
   /* USER CODE END USART2_Init 2 */
 
@@ -417,7 +467,7 @@ static void MX_GPIO_Init(void)
   LL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
-  NVIC_SetPriority(EXTI15_10_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),0, 0));
+  NVIC_SetPriority(EXTI15_10_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),5, 0));
   NVIC_EnableIRQ(EXTI15_10_IRQn);
 
 }
@@ -426,6 +476,99 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE END 4 */
 
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void *argument)
+{
+  /* USER CODE BEGIN 5 */
+    LL_USART_EnableIT_RXNE(USART1);
+    LL_USART_EnableIT_RXNE(USART2);
+  /* Infinite loop */
+  for(;;)
+  {
+      HAL_GPIO_TogglePin(LD2_GPIO_Port,LD2_Pin);
+      osDelay(500);
+  }
+  /* USER CODE END 5 */
+}
+
+/* USER CODE BEGIN Header_StartUart1Task */
+/**
+* @brief Function implementing the uart1Task thread. Receive byte from uart 1 stream buffer and send it to uart 2.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartUart1Task */
+void StartUart1Task(void *argument)
+{
+  /* USER CODE BEGIN StartUart1Task */
+    char rxBuffer[ 64 ];
+    size_t xReceivedBytes;
+    const TickType_t xBlockTime = pdMS_TO_TICKS( 10 );
+  /* Infinite loop */
+  for(;;)
+  {
+      ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+      xReceivedBytes = xStreamBufferReceive(uart1StreamBufferHandle,rxBuffer,sizeof (rxBuffer),xBlockTime);
+      if(xReceivedBytes > 0)
+      {
+          UART_Send(USART2,rxBuffer,xReceivedBytes);
+      }
+  }
+  /* USER CODE END StartUart1Task */
+}
+
+/* USER CODE BEGIN Header_StartUart2Task */
+/**
+* @brief Function implementing the uart2Task thread. Receive byte from uart 2 stream buffer and send it to uart 1.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartUart2Task */
+void StartUart2Task(void *argument)
+{
+  /* USER CODE BEGIN StartUart2Task */
+    char rxBuffer[ 64 ];
+    size_t xReceivedBytes;
+    const TickType_t xBlockTime = pdMS_TO_TICKS( 10 );
+    /* Infinite loop */
+    for(;;)
+    {
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+        xReceivedBytes = xStreamBufferReceive(uart2StreamBufferHandle,rxBuffer,sizeof (rxBuffer),xBlockTime);
+        if(xReceivedBytes > 0)
+        {
+            UART_Send(USART1,rxBuffer,xReceivedBytes);
+        }
+    }
+}
+
+ /**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM1 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM1) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
 /**
   * @brief  This function is executed in case of error occurrence.
   * @retval None
@@ -433,11 +576,11 @@ static void MX_GPIO_Init(void)
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
+    /* User can add his own implementation to report the HAL error return state */
+    __disable_irq();
+    while (1)
+    {
+    }
   /* USER CODE END Error_Handler_Debug */
 }
 
